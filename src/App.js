69125636b4199109c9bf15cd2c3228add106d61b
arifire21 <ariwolfpup@icloud.com>
2022-05-16 14:25:59 -0400

    new navbar, react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //IMPORT MODULES
 // import Hero from './components/Hero';
@@ -17,17 +18,17 @@ import './App.scss';
 
 import DesktopTabs from './OLD_ARCHIVED/DesktopTabs';
 import MobileTabs from './OLD_ARCHIVED/MobileTabs';
-import {BrowserView, MobileView} from 'react-device-detect';
+import { BrowserView, MobileView } from 'react-device-detect';
 import * as rdd from 'react-device-detect';
 import Footer from './components/Widgets/Footer';
-import NavTabs from './components/Widgets/NavTabs';
 import Logo from './images/logo.png'
-import CarouselNew from './components/Widgets/CarouselNew';
-import NavBar from './components/Widgets/NavBar';
 import BootstrapCarousel from './components/boostrap-components/bs-carousel';
+import BootstrapNavbar from './components/boostrap-components/bs-navbar';
 
 import MiniLogo from './images/logo192.png';
 import { Link } from 'react-router-dom';
+
+import Example from './components/boostrap-components/bs-navbar';
 
 function App() {
   //for testing mobile view
@@ -35,43 +36,29 @@ function App() {
 
   return (
     <div>
-    <div>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">
-            <img src={MiniLogo} alt="logo_small" width="50" height="50" />
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page"><Link className="nav-link-link" to='/'>Home</Link></a>
-              <a class="nav-link"><Link className="nav-link-link" to='/services'>Services</Link></a>
-              <a class="nav-link"><Link className="nav-link-link" to='/portfolio'>Project Portfolio</Link></a>
-              <a class="nav-link"><Link className="nav-link-link" to='/about'>About Us</Link></a>
-              <a class="nav-link"><Link className="nav-link-link" to='/contact'>Contact Us</Link></a>
-            </div>
-          </div>
+      <BootstrapNavbar/>
+      {/* <h1>Home</h1> */}
+
+
+      {/* <Example/> */}
+
+      <div className='home-container'>
+        <div className='home-item'>
+          <img id='logo' src={Logo} alt="lei logo" />
         </div>
-      </nav>
+        <div className='home-item'>
+          <BootstrapCarousel />
+        </div>
+      </div>
+
+      <hr className='titleheader' />
+
+      <p>company tagline</p>
+      <p>Loucks Engineering Inc. specializes in mechanical, electrical, and plumbing systems.</p>
+      <p>cool placeholder stuff</p>
+
+      <Footer/>
     </div>
-            <h1>Home</h1>
-            {/* <div className='home-container'>
-            <div className='home-item'>
-                <img id='logo' src={Logo} alt="lei logo"/>
-            </div>
-            <div className='home-item'>
-                <CarouselNew/>
-            </div>
-            </div>
-
-            <hr className='titleheader'/>
-
-            <p>company tagline</p>
-            <p>Loucks Engineering Inc. specializes in mechanical, electrical, and plumbing systems.</p>
-            <p>cool placeholder stuff</p> */}
-        </div>
   );
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,31 +1,9 @@
-import MiniLogo from '../images/logo192.png';
-import { Link } from 'react-router-dom';
+import BootstrapNavbar from '../components/boostrap-components/bs-navbar';
 
 export default function Contact() {
   return (
     <div>
-      <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="/">
-              <img src={MiniLogo} alt="logo_small" width="50" height="50" />
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <div class="navbar-nav">
-                <a class="nav-link"><Link className="nav-link-link" to='/'>Home</Link></a>
-                <a class="nav-link"><Link className="nav-link-link" to='/services'>Services</Link></a>
-                <a class="nav-link"><Link className="nav-link-link" to='/portfolio'>Project Portfolio</Link></a>
-                <a class="nav-link"><Link className="nav-link-link" to='/about'>About Us</Link></a>
-                <a class="nav-link active" aria-current="page"><Link className="nav-link-link" to='/contact'>Contact Us</Link></a>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </div>
-
+      <BootstrapNavbar/>
 
       <h1>Contact</h1>
     </div>
