33e0b1b3fd9acebaa043434cca3b9edb03101a4f
arifire21 <ariwolfpup@icloud.com>
2022-05-23 15:46:14 -0400

    portfolio landing page, cards

---
+++ src/components/boostrap-components/bs-portfolio-card.js
@@ -0,0 +1,21 @@
+import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export default function BootstrapFolioCard(props) {
+  return (
+    <div className='folio-card'>
+      <Card style={{ width: '18rem' }}>
+        {/* <Card.Img variant="top" src={props.img_src} /> */}
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>
+            {props.text}
+          </Card.Text>
+          <Link className='btn btn-danger' to={props.link}>
+            View Projects
+          </Link>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,6 +1,7 @@
 import BootstrapNavbar from '../components/boostrap-components/bs-navbar';
 import { ListGroup } from 'react-bootstrap';
 import Footer from '../components/Widgets/Footer';
+import BootstrapFolioCard from '../components/boostrap-components/bs-portfolio-card';
 // import LinkContainer from 'react-router-bootstrap/LinkContainer';
 
 export default function Portfolio() {
@@ -10,39 +11,84 @@ export default function Portfolio() {
 
       <h1>Portfolio</h1>
       <p>This is a portfolio overview page. description goes here</p>
-      <br/>
       <p>Click on items to be taken to portfolio pages.</p>
 
       <div className='portfolio-list-container'>
-        <div className='portfolio-column'>
-          <ListGroup variant="flush">
-            {/* <LinkContainer to="/portfolio/residences"> */}
-            <ListGroup.Item action href='/portfolio/residences'>Single-Family / Multifamily Residences</ListGroup.Item>
-            {/* </LinkContainer> */}
-            <ListGroup.Item action href='/portfolio/warehouses'>Warehouses</ListGroup.Item>
-            <ListGroup.Item>Office Buildings</ListGroup.Item>
-            <ListGroup.Item>Restaurants</ListGroup.Item>
-            <ListGroup.Item>Retail Spaces</ListGroup.Item>
-            <ListGroup.Item>Salons and Spas</ListGroup.Item>
-            <ListGroup.Item>Medical Centers</ListGroup.Item>
-            <ListGroup.Item>Hotels</ListGroup.Item>
-          </ListGroup>
-        </div>
-        <div className='portfolio-column'>
-          <ListGroup variant="flush">
-            <ListGroup.Item>Multi-Purpose</ListGroup.Item>
-            <ListGroup.Item>Schools/Colleges</ListGroup.Item>
-            <ListGroup.Item>Public Facilities</ListGroup.Item>
-            <ListGroup.Item>Telecommunications</ListGroup.Item>
-            <ListGroup.Item>Tenent Improvement</ListGroup.Item>
-            <ListGroup.Item>Media/Arts and Entertainment</ListGroup.Item>
-            <ListGroup.Item>Gymnasiums</ListGroup.Item>
-            <ListGroup.Item>8</ListGroup.Item>
-          </ListGroup>
-        </div>
+        <BootstrapFolioCard
+          // img_src=''
+          title='Single/Multifamily Residences'
+          text='Lorem ipsum dummy text'
+          link='/portfolio/residences'
+        />
+        <BootstrapFolioCard
+          // img_src=''
+          title='Restaurants'
+          text='Lorem ipsum dummy text'
+          link='/#'
+        />
+        <BootstrapFolioCard
+          // img_src=''
+          title='Retail Spaces'
+          text='Lorem ipsum dummy text'
+          link='/#'
+        />
+        <BootstrapFolioCard
+          // img_src=''
+          title='Tenent Improvements'
+          text='Lorem ipsum dummy text'
+          link='/#'
+        />
+        <BootstrapFolioCard
+          // img_src=''
+          title='Multi-Purpose'
+          text='Lorem ipsum dummy text'
+          link='/#'
+        />
+        <BootstrapFolioCard
+          // img_src=''
+          title='Office Spaces'
+          text='Lorem ipsum dummy text'
+          link='/#'
+        />
+        <BootstrapFolioCard
+          // img_src=''
+          title='Schools'
+          text='Lorem ipsum dummy text'
+          link='/#'
+        />
+        <BootstrapFolioCard
+          // img_src=''
+          title='Gymnasiums'
+          text='Lorem ipsum dummy text'
+          link='/#'
+        />
+        <BootstrapFolioCard
+          // img_src=''
+          title='Medical Offices'
+          text='Lorem ipsum dummy text'
+          link='/#'
+        />
+        <BootstrapFolioCard
+          // img_src=''
+          title='Media & Entertainment'
+          text='Lorem ipsum dummy text'
+          link='/#'
+        />
+        <BootstrapFolioCard
+          // img_src=''
+          title='TITLE'
+          text='Lorem ipsum dummy text'
+          link='/#'
+        />
+        <BootstrapFolioCard
+          // img_src=''
+          title='TITLE'
+          text='Lorem ipsum dummy text'
+          link='/#'
+        />
       </div>
 
-      <Footer/>
+      <Footer />
     </div>
   )
 }
