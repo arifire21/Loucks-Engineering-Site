b273cf494be5b3d985310e140814894ad154d5e4
arifire21 <ariwolfpup@icloud.com>
2022-05-24 16:59:42 -0400

    carousel styling

--- src/components/boostrap-components/bs-carousel.js
+++ src/components/boostrap-components/bs-carousel.js
@@ -10,45 +10,44 @@ import { Carousel, Image } from 'react-bootstrap';
 
 export default function BootstrapCarousel() {
   return (
-<Carousel fade variant="dark">
+<Carousel fade variant="light">
   <Carousel.Item>
     <img
-      className="fluid rounded-3 d-block"
+      className="fluid rounded-custom d-block"
       src={Oceanic_Img}
       alt="First slide"
       width='700'
       height='500'
     />
     <Carousel.Caption>
-      <h5>First slide label</h5>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <h5>Oceanic</h5>
+      <p>Pompano Pier</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
-      className="fluid rounded-3 d-block"
+      className="fluid rounded-custom d-block"
       src={Blu_Img}
       alt="Second slide"
       width='700'
       height='500'
     />
     <Carousel.Caption>
-      <h5>Second slide label</h5>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h5>Blu Apartments</h5>
+      <p>Marina Mile Blvd, Fort Lauderdale</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
-    {/* <Image src={Mora_Grill_img} fluid='true' rounded='true'/> */}
     <img
-      className="fluid rounded-3 d-block"
+      className="fluid rounded-custom d-block"
       src={Mora_Grill_img}
       alt="Third slide"
       width='700'
       height='500'
     />
     <Carousel.Caption>
-      <h5>Third slide label</h5>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <h5>Mora Grill</h5>
+      <p>Pomano Beach</p>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
