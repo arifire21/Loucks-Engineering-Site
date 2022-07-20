4ede46710a3eac945842a5627cfc712cdd70ade2
arifire21 <ariwolfpup@icloud.com>
2022-07-20 17:03:48 -0400

    sticky footer bc i dont care anymore

--- src/components/Widgets/Footer.js
+++ src/components/Widgets/Footer.js
@@ -5,63 +5,7 @@
 export default function Footer() {
     return (
     <footer>
-        <p><b>&copy; {new Date().getFullYear()}</b></p>
+        <p style={{marginBottom: '0'}}><b>&copy; {new Date().getFullYear()}</b></p>
     </footer>
-
-        // <footer>
-        //     <Box bgcolor="gray"
-        //         color="white"
-        //         px={{ xs: 5, sm: 5 }}
-        //         py={{ xs: 5, sm: 5 }}
-        //     >
-
-        //         <Container maxWidth="lg">
-        //             <Grid container spacing={6}>
-        //                 <Grid item xs={8} sm={3}>
-        //                     <Box> <img src={Logo} alt="lei logo" width='220' height='100' /></Box>
-        //                 </Grid>
-        //                 <Grid item xs={8} sm={4}>
-        //                     <Box borderBottom={1}><b>ItemOne</b></Box>
-        //                     <Box>
-        //                         <p>Filler text about company here??</p>
-        //                         <p>Line test</p>
-        //                         <p>Line test</p>
-        //                     </Box>
-        //                     {/*
-        //                     <Box>
-        //                         <Link href="url" underline="always" color="inherit">LinkText</Link>
-        //                     </Box> */}
-        //                 </Grid>
-        //                 {/* <Grid item xs={12} sm={4}> 
-        //                     <Box borderBottom={1}><b>ItemTwo</b></Box>
-        //                     <Box>
-        //                         <Link href="url" underline="always" color="inherit">LinkText</Link>
-        //                     </Box>
-        //                     <Box>
-        //                         <Link href="url" underline="always" color="inherit">LinkText</Link>
-        //                     </Box>
-        //                 </Grid> */}
-        //                 <Grid item xs={8} sm={4}>
-        //                     <Box borderBottom={1}><b>Contact Us</b></Box>
-        //                     <Box>
-        //                         <p><b>Phone</b>: (123) 456-7890</p>
-        //                     </Box>
-        //                     <Box>
-        //                         <p><b>Fax</b>: (123) 456-7890</p>
-        //                     </Box>
-        //                     <Box>
-        //                         <img src={Pin} alt="maps_pin" width='20' height='30' className="maps-pin" />
-        //                         <Link href="https://goo.gl/maps/vfK9qLgASjXJTs7r5" underline="always" color="inherit" target="_blank"><b>Address</b></Link>
-        //                     </Box>
-        //                 </Grid>
-        //             </Grid>
-        //             {/* copyright */}
-        //             <Box textAlign="center">
-        //                 &copy; {new Date().getFullYear()}
-        //                 {/* Maintained by _name_ */}
-        //             </Box>
-        //         </Container>
-        //     </Box>
-        // </footer>
     )
 }
