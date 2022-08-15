2392acd351911ca9b7aadf21fe215e6cb1ad84f2
arifire21 <ariwolfpup@icloud.com>
2022-08-14 20:20:21 -0400

    pruning portfolio pages

--- src/pages/portfolio/aviation.js
+++ src/pages/portfolio/aviation.js
@@ -3,25 +3,30 @@ import Footer from '../../components/Widgets/Footer';
 import Helmet from 'react-helmet';
 import PortfolioReturnBtn from "../../components/boostrap-components/bs-portfolio-btn";
 
-export default function Tenents() {
+export default function Aviation() {
   return (
     <div>
       <Helmet>
-      <title>Tenent Improvements - Portfolio | Loucks Engineering Inc.</title>  
+      <title>Aviation Services - Portfolio | Loucks Engineering Inc.</title>  
       </Helmet>
 
       <BootstrapNavbar/>
 
       <div className="page-contents">
         <header>
-          <h1>Tenent Improvements</h1>
+          <h1>Aviation Services</h1>
           <p>description text here</p>
         </header>
 
        <div>
           
 
-          <p className="project-name">Project Name</p>
+          <p className="project-name">Avenger Flight Group</p>
+          <ul>
+            <li>Phase 1 and 2 Revisions</li>
+            <li>Flight Simulator</li>
+            <li>AC</li>
+          </ul>
           <p>date | work done</p>
 
           <p className="project-name">Project Name</p>
