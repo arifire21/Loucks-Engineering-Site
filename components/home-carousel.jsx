17e1eada29b656f55725c1c8c7f592c20d998e16
arifire21 <ariwolfpup@icloud.com>
2025-08-07 16:46:19 -0400

    override caro tab-index, pretty up arrow styling

--- components/home-carousel.jsx
+++ components/home-carousel.jsx
@@ -1,9 +1,29 @@
 import {Carousel} from 'antd';
 import {carouselData} from '../data/carousel_data';
 import styles from '@/styles/carousel.module.scss'
+
+function initTabIndex() {
+  console.log("caro init started")
+  document.querySelectorAll('.slick-dots li button').forEach((dot) => {
+    console.log("button found")
+    dot.tabIndex = "-1";
+    console.log("btn set tabindex -1");
+  //   dot.addEventListener('keydown', (e) => {
+  //     if (e.key === 'Tab') {
+  //       dot.classList.add('focus-visible');
+  //     }
+  //   });
+
+  //   dot.addEventListener('blur', () => {
+  //     dot.classList.remove('focus-visible');
+  //   });
+  });
+  console.log("caro init ended")
+}
+
 export default function HomeCarousel(){
     return(
-        <Carousel className={styles.caroWrapper} arrows dots dotPosition='top' autoplay={{ dotDuration: true }} autoplaySpeed={4500} infinite>
+        <Carousel onInit={initTabIndex} className={styles.caroWrapper} arrows dots dotPosition='top' autoplay={{ dotDuration: true }} autoplaySpeed={4500} infinite>
           {carouselData.map((entry, index) =>
               <div key={index}>
                 <img className={styles.caroImage}
