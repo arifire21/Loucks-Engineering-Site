0ba814b0c54b51345034f5412000c6bcabb118da
arifire21 <ariwolfpup@icloud.com>
2025-08-22 19:55:39 -0400

    working sticky nav with weird nested behavior

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -7,10 +7,12 @@ export default function Portfolio() {
   //need to have a static height for sticky nav to work
   // useEffect(() => {
   //   if(typeof window !== "undefined"){
-  //     const portfolioPageLayout = document.getElementsByClassName("portfolio-page")[0] as HTMLElement;
+  //     const portfolioPageLayout = document.getElementById("portfolio-main") as HTMLElement;
   //     console.log("client height: ", portfolioPageLayout!.clientHeight)
-  //     console.log("scroll height: ", portfolioPageLayout!.scrollHeight)
+  //     // console.log("scroll height: ", portfolioPageLayout!.scrollHeight)
   //     portfolioPageLayout!.style.height = portfolioPageLayout!.clientHeight + 'px';
+
+  //     console.log(portfolioPageLayout)
   //   }
   // }, []);
 
@@ -22,7 +24,7 @@ export default function Portfolio() {
         <meta name="description" content="View a gallery showcasing notable projects." />
         </Head>
 
-        {/* <main id="portfolio-main"> */}
+        <div id="portfolio-main">
           <h1>Project Portfolio</h1>
           <p>View a gallery showcasing notable projects.</p>
 
@@ -33,11 +35,11 @@ export default function Portfolio() {
 
           <hr />
 
-          <section style={{ margin: "1rem 2rem", textAlign: 'left' }}>
+          <section style={{ margin: "1rem 2rem", textAlign: 'left', fontSize:'12pt' }}>
               <p><span style={{ color: 'darkred', fontWeight: 'bold' }}>Disclaimer:</span> Pictures are used to indicate the spaces on location of listed projects. Projects include tenant improvements, shell buildings, remodels, and complete jobs. By using them, we are not intending to misrepresent our projects.
                   Pictures used are either provided by us or pulled from the general internet. We believe that no copyright laws have been violated. If we are notified differently, we will immediately remove the picture(s) in question.</p>
           </section>
-        {/* </main> */}
+        </div>
       </>
     )
 }
