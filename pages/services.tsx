c01928aa64d50b6a9892e1ea727cc5d49ba33974
arifire21 <afloucks@outlook.com>
2025-08-28 09:32:42 -0400

    defining more viewport breakpoints for zoom (#33)

--- pages/services.tsx
+++ pages/services.tsx
@@ -16,9 +16,9 @@ export default function Services() {
         <p>We provide our services to various types of projects, from sizable warehouses to small single-family residences.</p>
 
         <div className={styles.serviceFlexContainer}>
-          <div className={styles.item}>
-            <p className={styles.subtitle}>Mechanical</p>
-            <hr className="service-header"/>
+          <section className={styles.item}>
+            <h2 className={styles.subtitle}>Mechanical</h2>
+            <hr className={styles.serviceHeader}/>
             <ul className={styles.list}>
               <li>HVAC Design</li>
               <li>HVAC Load Calculations</li>
@@ -28,11 +28,11 @@ export default function Services() {
               <li>System Design for Enclosure Airflow Control<br/>
               <small>(physical enclosure spaces designed by others)</small></li>
             </ul>
-          </div>
+          </section>
 
-          <div className={styles.item}>
-            <p className={styles.subtitle}>Electrical</p>
-            <hr className="service-header"/>
+          <section className={styles.item}>
+            <h2 className={styles.subtitle}>Electrical</h2>
+            <hr className={styles.serviceHeader}/>
             <ul className={styles.list}>
               <li>Power Distribution</li>
               <li>Lighting Design and Controls</li>
@@ -40,18 +40,18 @@ export default function Services() {
               <li>Photometrics</li>
               <li>UPS Systems</li>
             </ul>
-          </div>
+          </section>
 
-          <div className={styles.item}>
-            <p className={styles.subtitle}>Plumbing</p>
-            <hr className="service-header"/>
+          <section className={styles.item}>
+            <h2 className={styles.subtitle}>Plumbing</h2>
+            <hr className={styles.serviceHeader}/>
             <ul className={styles.list}>
               <li>Plumbing Systems Design</li>
               <li>Domestic Water System Design</li>
               <li>Sanitary Waste System Design</li>
               <li>Natural Gas System Design</li>
             </ul>
-          </div>
+          </section>
         </div>
 
         <Button href='/portfolio' variant="filled" type="primary">See our work</Button>
