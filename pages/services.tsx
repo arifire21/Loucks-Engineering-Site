a5dbb15bc57ea3b862c725b75e005709858114b4
arifire21 <afloucks@outlook.com>
2026-01-10 17:59:55 -0500

    merge conflict resolved (#35)

--- pages/services.tsx
+++ pages/services.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Button } from "antd";
+import { Button, Card } from "antd";
 import styles from '@/styles/services.module.scss';
 
 export default function Services() {
@@ -15,24 +15,21 @@ export default function Services() {
         <h1>Services Provided</h1>
         <p>We provide our services to various types of projects, from sizable warehouses to small single-family residences.</p>
 
-        <div className={styles.serviceFlexContainer}>
-          <section className={styles.item}>
-            <h2 className={styles.subtitle}>Mechanical</h2>
-            <hr className={styles.serviceHeader}/>
-            <ul className={styles.list}>
-              <li>HVAC Design</li>
-              <li>HVAC Load Calculations</li>
+        <main className={styles.cardContainer}>
+          <Card title="Mechanical" className={styles.cardCustom}>
+          <ul className={styles.list}>
+              <li>New HVAC Design & Load Calculations</li>
               <li>Existing HVAC System Analysis</li>
               <li>Building Energy Audits</li>
-              <li>Manufacturing Facility/Cleanroom Ductwork</li>
+              <li>Manufacturing Facility or Cleanroom Ductwork Design</li>
               <li>System Design for Enclosure Airflow Control<br/>
-              <small>(physical enclosure spaces designed by others)</small></li>
+                <small>(physical enclosure spaces designed by others)</small>
+              </li>
             </ul>
-          </section>
+          </Card>
 
-          <section className={styles.item}>
-            <h2 className={styles.subtitle}>Electrical</h2>
-            <hr className={styles.serviceHeader}/>
+          <Card title="Electrical" className={styles.cardCustom}>
+            <div className=""></div>
             <ul className={styles.list}>
               <li>Power Distribution</li>
               <li>Lighting Design and Controls</li>
@@ -40,19 +37,18 @@ export default function Services() {
               <li>Photometrics</li>
               <li>UPS Systems</li>
             </ul>
-          </section>
+          </Card>
 
-          <section className={styles.item}>
-            <h2 className={styles.subtitle}>Plumbing</h2>
-            <hr className={styles.serviceHeader}/>
-            <ul className={styles.list}>
+          <Card title="Plumbing" className={styles.cardCustom}>
+          <ul className={styles.list}>
               <li>Plumbing Systems Design</li>
               <li>Domestic Water System Design</li>
               <li>Sanitary Waste System Design</li>
               <li>Natural Gas System Design</li>
             </ul>
-          </section>
-        </div>
+          </Card>
+
+        </main>
 
         <Button href='/portfolio' variant="filled" type="primary">See our work</Button>
       </main>
