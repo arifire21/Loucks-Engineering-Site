9dd92cfbdc4001d4d0398f12052905a7e055b8a6
arifire21 <ariwolfpup@icloud.com>
2026-01-10 17:58:23 -0500

    merge conflict resolved

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -19,7 +19,7 @@ export default function Contact() {
         <div className={styles.cardContainer}>
         <Card title="Office Location" className={styles.cardCustom}>
         <p style={{marginBottom:0}}>321 E. Commercial Blvd<br/>Fort Lauderdale, FL 33334</p>
-        <a style={{color: 'darkred', textDecoration: 'underline'}} href="https://goo.gl/maps/vfK9qLgASjXJTs7r5" target="_blank" rel="noreferrer">Directions via Google Maps <HiOutlineExternalLink/></a>
+        <a className={styles.mailLink} href="https://goo.gl/maps/vfK9qLgASjXJTs7r5" target="_blank" rel="noreferrer">Directions via Google Maps <HiOutlineExternalLink/></a>
         </Card>
 
         <Card title="Hours of Operation" className={styles.cardCustom}>
